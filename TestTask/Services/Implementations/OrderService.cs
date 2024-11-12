103221a1bf2d1475545fb67ed913f11edb98ef84
lesha <lesha@DESKTOP-F31VJOV>
2024-11-12 16:32:05 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+        private const int minQuantity = 1;
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _context.Orders.Where(x => x.User.Status == Enums.UserStatus.Active).OrderBy(x => x.CreatedAt).ToListAsync();
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            return await _context.Orders.Where(x => x.Quantity > minQuantity).OrderByDescending(x => x.CreatedAt).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        private const int orderYear = 2010;
+        private const int orderYearSecondTask = 2003;
+        public UserService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users.Where(x => x.Orders.Any(x => x.Status == Enums.OrderStatus.Paid && x.CreatedAt.Year == orderYear)).ToListAsync();
+        }
+
+        public async Task<User> GetUser()
+        {
+            var user = await Task.Run(() =>
+                _context.Orders.Where(x => x.Status == OrderStatus.Delivered && x.CreatedAt.Year == orderYearSecondTask)
+                .GroupBy(o => o.UserId).Select(o => new { UserId = o.Key, OrderCount = o.Count() }).OrderByDescending(o => o.OrderCount).FirstOrDefault());
+
+            return await _context.Users.Where(x => x.Id == user.UserId).FirstOrDefaultAsync();
+        }
+    }
+}
